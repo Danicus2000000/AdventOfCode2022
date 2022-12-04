8325f187b15cd00b2074ed31f8e1b41edc026705
Dan <dbulman68@gmail.com>
2022-12-04 23:05:44 +0000

    started day 3

---
+++ Day3/Day3/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+string rawData = File.ReadAllText("puzzleData.txt");
+Dictionary<string,string> rucksacks= new Dictionary<string,string>();
+string[] rucksackRaw = rawData.Split("\r\n");
+foreach(string sack in rucksackRaw) 
+{
+    rucksacks.Add(sack.Substring(0, sack.Length / 2), sack.Substring(sack.Length / 2));
+}
