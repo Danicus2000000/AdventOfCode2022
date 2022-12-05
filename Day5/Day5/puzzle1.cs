f08f0bc8e602e0090d3f0fce4e9c1a293e2d33f0
Danicus2000000 <66948931+Danicus2000000@users.noreply.github.com>
2022-12-05 16:47:26 +0000

    Made bugged day 5 puzzle 1

--- Day5/Day5/puzzle1.cs
+++ Day5/Day5/puzzle1.cs
@@ -47,5 +47,72 @@ The Elves just need to know which crate will end up on top of each stack; in thi
 
 After the rearrangement procedure completes, what crate ends up on top of each stack?*/
 string rawPuzzle = File.ReadAllText("puzzleData.txt");
-puzzle2 puzzle2= new puzzle2();
+List<string> rawPuzzleLines = rawPuzzle.Split("\r\n").ToList();
+List<Stack<char>> crates = new List<Stack<char>> { new Stack<char>(), new Stack<char>(), new Stack<char>(), new Stack<char>(), new Stack<char>(), new Stack<char>(), new Stack<char>(), new Stack<char>(), new Stack<char>() };
+foreach(string line in rawPuzzleLines) 
+{
+    if (line.Contains('1')) { break; }
+    if (line[1] != ' ') 
+    {
+        crates[0].Push(line[1]);
+    }
+    if (line[5] != ' ')
+    {
+        crates[1].Push(line[5]);
+    }
+    if (line[9] != ' ')
+    {
+        crates[2].Push(line[9]);
+    }
+    if (line[13] != ' ')
+    {
+        crates[3].Push(line[13]);
+    }
+    if (line[17] != ' ')
+    {
+        crates[4].Push(line[17]);
+    }
+    if (line[21] != ' ')
+    {
+        crates[5].Push(line[21]);
+    }
+    if (line[25] != ' ')
+    {
+        crates[6].Push(line[25]);
+    }
+    if (line[29] != ' ')
+    {
+        crates[7].Push(line[29]);
+    }
+    if (line[33] != ' ')
+    {
+        crates[8].Push(line[33]);
+    }
+}
+for(int i=0;i<crates.Count;i++) //reverses stacks so the top of the crate pile can be pulled correctly
+{
+    crates[i]=new Stack<char>(crates[i]);
+}
+rawPuzzleLines.RemoveRange(0, 10);//converts to instruction list
+foreach(string instruction in rawPuzzleLines) 
+{
+    string[] instructionset=instruction.Split(' ');
+    List<int> trueRequirments=new List<int>();
+    foreach(string word in instructionset) 
+    {
+        if(int.TryParse(word,out int requiredValue)) 
+        {
+            trueRequirments.Add(requiredValue);
+        }
+    } 
+    int quantity = trueRequirments[0];
+    int from = trueRequirments[1]-1;
+    int to = trueRequirments[2]-1;
+    for(int i = 0; i < quantity; i++) 
+    {
+        crates[to].Push(crates[from].Pop());
+    }
+}
+Console.WriteLine("Crates on top in order: " + crates[0].Last()+" "+ crates[1].Last() + " " + crates[2].Last() + " " + crates[3].Last() + " " + crates[4].Last() + " " + crates[5].Last() + " " + crates[6].Last() + " " + crates[7].Last() + " " + crates[8].Last() + " ");
+puzzle2 puzzle2 = new puzzle2();
 puzzle2.main();
