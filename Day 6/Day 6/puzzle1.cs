8f3c87db401dcb1bdbaf64370636993b1030e989
Dan <dbulman68@gmail.com>
2022-12-12 01:40:03 +0000

    Completed day 6

---
+++ Day 6/Day 6/puzzle1.cs
@@ -0,0 +1,34 @@
+﻿using Day_6;
+
+string dataIn = File.ReadAllText("puzzleData.txt");
+string[] currentCheck = {"","","","" };
+int next = 0;
+int index = 1;
+foreach (char cur in dataIn)
+{
+    currentCheck[next] = cur.ToString();
+    if(!currentCheck.Contains(""))
+    {
+        bool isUnique = true;
+        for (int i = 0; i < currentCheck.Length; i++) 
+        {
+            for (int j = 0; j < currentCheck.Length; j++)
+            {
+                if (currentCheck[i] == currentCheck[j] && i!=j)
+                {
+                    isUnique = false;
+                }
+            }
+        }
+        if (isUnique) 
+        {
+            Console.WriteLine("First unique start of packet marker found at: " + index);
+            break;
+        }
+    }
+    if (next == 3) { next = 0; }
+    else { next++; }
+    index++;
+}
+puzzle2 puz = new puzzle2();
+puz.main();
---
+++ Day 6/Day 6/puzzle2.cs
@@ -0,0 +1,39 @@
+﻿namespace Day_6
+{
+    internal class puzzle2
+    {
+        internal void main() 
+        {
+            string dataIn = File.ReadAllText("puzzleData.txt");
+            string[] currentCheck = { "", "", "","","","","","","","","","","","" };
+            int next = 0;
+            int index = 1;
+            foreach (char cur in dataIn)
+            {
+                currentCheck[next] = cur.ToString();
+                if (!currentCheck.Contains(""))
+                {
+                    bool isUnique = true;
+                    for (int i = 0; i < currentCheck.Length; i++)
+                    {
+                        for (int j = 0; j < currentCheck.Length; j++)
+                        {
+                            if (currentCheck[i] == currentCheck[j] && i != j)
+                            {
+                                isUnique = false;
+                            }
+                        }
+                    }
+                    if (isUnique)
+                    {
+                        Console.WriteLine("First unique start of message marker found at: " + index);
+                        break;
+                    }
+                }
+                if (next == 13) { next = 0; }
+                else { next++; }
+                index++;
+            }
+        }
+    }
+}
