3fdaf3775bf19e7ebc1cfedd8fba0b1781b1f1e5
Dan <dbulman68@gmail.com>
2022-12-12 22:12:23 +0000

    Made progress on day 7 and improved day 5 code

--- Day 7/Day 7/puzzle1.cs
+++ Day 7/Day 7/puzzle1.cs
@@ -1,18 +1,18 @@
 ﻿using Day_7;
 
-string data = File.ReadAllText("puzzleData.txt");
-string[] consoleLines = data.Split("\r\n");
-List<Dictionary<string, int>> directorySizes = new List<Dictionary<string, int>>();
+string data = File.ReadAllText("puzzleData.txt");//gets text file data
+string[] consoleLines = data.Split("\r\n");//splits input by line
+Dictionary<string, int> directorySizes = new Dictionary<string, int>();//creates dictionary lookup of addresses to size
 string curDirectory = "";
-for (int i=0;i<consoleLines.Length;i++) 
+for (int i=0;i<consoleLines.Length;i++) //loops through all instructions
 {
-    if (consoleLines[i].Contains("$"))
+    if (consoleLines[i].Contains("$"))//if it is a command
     {
         if (consoleLines[i].Contains("cd /"))//go to base directory
         {
             curDirectory = "/";
         }
-        else if (consoleLines[i].Contains("cd .."))//go back a directory
+        else if (consoleLines[i].Contains("cd .."))//go back a directory if we are not in base directory
         {
             if (curDirectory != "/")
             {
@@ -24,15 +24,51 @@ for (int i=0;i<consoleLines.Length;i++)
         {
             curDirectory += consoleLines[i].Replace("$ cd ", "")+"/";
         }
-        else if (consoleLines[i].Contains("ls"))
+        if (consoleLines[i].Contains("ls"))//if we want to list files in curent directory
         {
-            while (!consoleLines[i].Contains("$")) 
+            for(int j = i + 1;j!=consoleLines.Length && !consoleLines[j].Contains("$");j++)//loop through lines until the next command or hit end of console lines
             {
-
+                if (consoleLines[j].Contains("dir"))//if a directory is found do nothing
+                {
+                    //nothing to see here
+                }
+                else//increments size of current directory if it exists otherwise adds it
+                {
+                    int size = Convert.ToInt32(consoleLines[j].Split(" ")[0]);
+                    string filename = consoleLines[j].Split(" ")[1];
+                    if (directorySizes.ContainsKey(curDirectory))
+                    {
+                        directorySizes[curDirectory] += size;
+                    }
+                    else 
+                    {
+                        directorySizes.Add(curDirectory, size);
+                    }
+                }
             }
         }
-        Console.WriteLine(curDirectory);
     }
 }
+for (int i = 0; i < directorySizes.Count; i++)//adds sub directories to parent directories counter
+{
+    for (int j = 0; j < directorySizes.Count; j++)
+    {
+        if (i != j && directorySizes.ElementAt(j).Key.Contains(directorySizes.ElementAt(i).Key))
+        {
+            directorySizes[directorySizes.ElementAt(i).Key] += directorySizes.ElementAt(j).Value;
+        }
+    }
+}
+int totalSum = 0;
+Console.WriteLine("The directories less than 100000:");
+foreach (KeyValuePair<string,int> value in directorySizes) //print out all directories values
+{
+    if (value.Value <= 100000)
+    {
+        Console.WriteLine(value.Key + ": " + value.Value);
+        totalSum += value.Value;
+    }
+}
+Console.WriteLine("The sum of those directories: " + totalSum);
 puzzle2 puz = new puzzle2();
 puz.main();
--- Day5/Day5/puzzle.cs
+++ Day5/Day5/puzzle.cs
@@ -1,25 +1,29 @@
 ﻿using System.Collections;
 using System.Text.RegularExpressions;
 string[] inputGroups = File.ReadAllText("puzzleData.txt").Split("\r\n\r\n");
-string[] moveInstructions = inputGroups[1].Split(Environment.NewLine);
-string[] crateRows = inputGroups[0].Split(Environment.NewLine);
+string[] moveInstructions = inputGroups[1].Split("\r\n");
+string[] crateRows = inputGroups[0].Split("\r\n");
 string crateNumberRow = crateRows.Last();
 List<int> crateLabelPositions = GetLabelPositions(crateNumberRow);
 Dictionary<int,Stack> crateStacks = PopulateCrateStacks(crateLabelPositions, crateRows);
 
 // Puzzle 1
-foreach (var moveInstruction in moveInstructions)
+foreach (string moveInstruction in moveInstructions)
 {
     var (transferCount, fromStack, toStack) = ParseInstructions(moveInstruction);
-    for (var i = 0; i < transferCount; i++)
+    for (int i = 0; i < transferCount; i++)
     {
-        var crate = crateStacks[fromStack].Pop();
+        Object crate = crateStacks[fromStack].Pop();
         crateStacks[toStack].Push(crate);
     }
 }
 
-var topCrates = string.Join("", crateStacks.Select(crateStack => (char)(crateStack.Value.Peek() ?? "")).ToList());
-Console.WriteLine($"Puzzle 1 has crates: {topCrates} at the top");
+string topCrates = "";
+foreach (KeyValuePair<int,Stack> keyValuePairin in crateStacks) 
+{
+    topCrates += keyValuePairin.Value.Peek();
+}
+Console.WriteLine("Puzzle 1 has crates: "+topCrates+" at the top");
 
 // Puzzle 2
 crateStacks.Clear();
@@ -30,61 +34,54 @@ foreach (var moveInstruction in moveInstructions)
 {
     var (transferCount, fromStack, toStack) = ParseInstructions(moveInstruction);
     // Reverse move crates transferCount number of times
-    crateStacks[fromStack].ToArray()
-        .Take(transferCount)
-        .Reverse().ToList()
-        .ForEach(supply =>
-        {
-            crateStacks[fromStack].Pop();
-            crateStacks[toStack].Push(supply);
-        });
+    crateStacks[fromStack].ToArray().Take(transferCount).Reverse().ToList().ForEach(supply =>{crateStacks[fromStack].Pop();crateStacks[toStack].Push(supply);});
 }
-
-topCrates = string.Join("", crateStacks.Select(crateStack => (char)(crateStack.Value.Peek() ?? "")).ToList());
-Console.WriteLine($"Puzzle 2 has crates: {topCrates} at the top");
+topCrates = "";
+foreach (KeyValuePair<int, Stack> keyValuePairin in crateStacks)
+{
+    topCrates += keyValuePairin.Value.Peek();
+}
+Console.WriteLine("Puzzle 2 has crates: "+topCrates+" at the top");
 
 // functions
 
 (int transferCount, int fromStack, int toStack) ParseInstructions(string instruction)
 {
-    var instructionParts = Regex.Matches(instruction, @"\d+")
-        .Select(m => m.Value)
-        .ToArray();
-    var transferCount = int.Parse(instructionParts[0]);
-    var fromStack = int.Parse(instructionParts[1]);
-    var toStack = int.Parse(instructionParts[2]);
+    string[] instructionParts = Regex.Matches(instruction, @"\d+").Select(m => m.Value).ToArray();
+    int transferCount = int.Parse(instructionParts[0]);
+    int fromStack = int.Parse(instructionParts[1]);
+    int toStack = int.Parse(instructionParts[2]);
     return (transferCount, fromStack, toStack);
 }
 
 Dictionary<int, Stack> PopulateCrateStacks(IReadOnlyList<int> labelPositions, IReadOnlyList<string> supplyCratesInput)
 {
-    var crateStacks = new Dictionary<int, Stack>();
-    for (var i = 0; i < labelPositions.Count; i++) crateStacks.Add(i + 1, new Stack());
-
-    for (var i = 0; i < labelPositions.Count; i++)
+    Dictionary<int,Stack> crateStacks = new Dictionary<int, Stack>();
+    for (int i = 0; i < labelPositions.Count; i++) 
     {
-        var alphabetPosition = labelPositions[i];
-        // for each crate row, except the last one, add crate labels to stack
-        for (var j = supplyCratesInput.Count - 1; j >= 0; j--)
-        {
-            var crateRow = supplyCratesInput[j];
-            var crateLabel = crateRow[alphabetPosition];
-            if (char.IsLetter(crateLabel)) crateStacks[i + 1].Push(crateLabel);
-        }
+        crateStacks.Add(i + 1, new Stack());
     }
 
+    for (int i = 0; i < labelPositions.Count; i++)
+    {
+        int alphabetPosition = labelPositions[i];
+        // for each crate row, except the last one, add crate labels to stack
+        for (int j = supplyCratesInput.Count - 1; j >= 0; j--)
+        {
+            string crateRow = supplyCratesInput[j];
+            char crateLabel = crateRow[alphabetPosition];
+            if (char.IsLetter(crateLabel)) 
+            { 
+                crateStacks[i + 1].Push(crateLabel); 
+            }
+        }
+    }
     return crateStacks;
 }
 
 List<int> GetLabelPositions(string row)
 {
-    var positions = new List<int>();
-    row
-        .Select((c, idx) => new { character = c, index = idx })
-        .ToList()
-        .ForEach(c =>
-        {
-            if (char.IsDigit(c.character)) positions.Add(c.index);
-        });
+    List<int> positions = new List<int>();
+    row.Select((c, idx) => new { character = c, index = idx }).ToList().ForEach(c =>{if (char.IsDigit(c.character)) positions.Add(c.index);});
     return positions;
 }
