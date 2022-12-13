9a9a04f22815e74df22c68334bb838acc52eb2c5
Dan <dbulman68@gmail.com>
2022-12-13 23:49:30 +0000

    Completed Day 8

--- Day 8/Day 8/puzzle1.cs
+++ Day 8/Day 8/puzzle1.cs
@@ -1,4 +1,27 @@
-﻿namespace Day_8
+﻿/*The Elves have already launched a quadcopter to generate a map with the height of each tree (your puzzle input). For example:
+
+30373
+25512
+65332
+33549
+35390
+Each tree is represented as a single digit whose value is its height, where 0 is the shortest and 9 is the tallest.
+
+A tree is visible if all of the other trees between it and an edge of the grid are shorter than it. Only consider trees in the same row or column; that is, only look up, down, left, or right from any given tree.
+
+All of the trees around the edge of the grid are visible - since they are already on the edge, there are no trees to block the view. In this example, that only leaves the interior nine trees to consider:
+
+The top-left 5 is visible from the left and top. (It isn't visible from the right or bottom since other trees of height 5 are in the way.)
+The top-middle 5 is visible from the top and right.
+The top-right 1 is not visible from any direction; for it to be visible, there would need to only be trees of height 0 between it and an edge.
+The left-middle 5 is visible, but only from the right.
+The center 3 is not visible from any direction; for it to be visible, there would need to be only trees of at most height 2 between it and an edge.
+The right-middle 3 is visible from the right.
+In the bottom row, the middle 5 is visible, but the 3 and 4 are not.
+With 16 trees visible on the edge and another 5 visible in the interior, a total of 21 trees are visible in this arrangement.
+
+Consider your map; how many trees are visible from outside the grid?*/
+namespace Day_8
 {
     internal static class puzzle1
     {
@@ -16,7 +39,7 @@
                 }
                 grid.Add(toAdd);
             }
-            int totalViewable = 0;
+            int totalViewable = 0;//gets total viewable trees
             for(int i=0;i<grid.Count;i++) 
             {
                 for (int j = 0; j < grid[i].Count; j++) 
@@ -31,12 +54,8 @@
         }
         public static bool checkViewable(List<List<int>> grid,int row,int col)//check if a tree is viewable
         {
-            bool rightClear = true;
-            bool leftClear = true;
-            bool upClear = true;
-            bool downClear = true;
             bool isOutside= false;
-            if (row - 1 == -1 || row+1==grid.Count+1 || col-1==-1 || col + 1 == grid[row].Count+1) 
+            if (row - 1 == -1 || row+1==grid.Count || col-1==-1 || col + 1 == grid[row].Count) 
             {
                 isOutside= true;
             }
@@ -46,20 +65,68 @@
             }
             return false;
         }
-        public static bool checkUp(List<List<int>> grid, int row, int col) 
+        public static bool checkUp(List<List<int>> grid, int row, int col) //checks up to see if view is blocked
         {
+            int tallestTreeAhead = 0;
+            for (int i = row-1; i != -1; i--) 
+            {
+                if (grid[i][col] > tallestTreeAhead) 
+                {
+                    tallestTreeAhead = grid[i][col];
+                }
+            }
+            if (grid[row][col] > tallestTreeAhead) 
+            {
+                return true;
+            }
             return false;
         }
-        public static bool checkDown(List<List<int>> grid, int row, int col)
+        public static bool checkDown(List<List<int>> grid, int row, int col)//checks down to see if view is blocked
         {
+            int tallestTreeAhead = 0;
+            for (int i = row + 1; i!=grid.Count; i++)
+            {
+                if (grid[i][col] > tallestTreeAhead)
+                {
+                    tallestTreeAhead = grid[i][col];
+                }
+            }
+            if (grid[row][col] > tallestTreeAhead)
+            {
+                return true;
+            }
             return false;
         }
-        public static bool checkLeft(List<List<int>> grid, int row, int col)
+        public static bool checkLeft(List<List<int>> grid, int row, int col)//checks left to see if view is blocked
         {
+            int tallestTreeAhead = 0;
+            for (int j = col - 1; j != -1; j--)
+            {
+                if (grid[row][j] > tallestTreeAhead)
+                {
+                    tallestTreeAhead = grid[row][j];
+                }
+            }
+            if (grid[row][col] > tallestTreeAhead)
+            {
+                return true;
+            }
             return false;
         }
-        public static bool checkRight(List<List<int>> grid, int row, int col)
+        public static bool checkRight(List<List<int>> grid, int row, int col)//checks right to see if view is blocked
         {
+            int tallestTreeAhead = 0;
+            for (int j = col + 1; j != grid.Count; j++)
+            {
+                if (grid[row][j] > tallestTreeAhead)
+                {
+                    tallestTreeAhead = grid[row][j];
+                }
+            }
+            if (grid[row][col] > tallestTreeAhead)
+            {
+                return true;
+            }
             return false;
         }
 
--- Day 8/Day 8/puzzle2.cs
+++ Day 8/Day 8/puzzle2.cs
@@ -1,10 +1,147 @@
-﻿namespace Day_8
+﻿/*Content with the amount of tree cover available, the Elves just need to know the best spot to build their tree house: they would like to be able to see a lot of trees.
+
+To measure the viewing distance from a given tree, look up, down, left, and right from that tree; stop if you reach an edge or at the first tree that is the same height or taller than the tree under consideration. (If a tree is right on the edge, at least one of its viewing distances will be zero.)
+
+The Elves don't care about distant trees taller than those found by the rules above; the proposed tree house has large eaves to keep it dry, so they wouldn't be able to see higher than the tree house anyway.
+
+In the example above, consider the middle 5 in the second row:
+
+30373
+25512
+65332
+33549
+35390
+Looking up, its view is not blocked; it can see 1 tree (of height 3).
+Looking left, its view is blocked immediately; it can see only 1 tree (of height 5, right next to it).
+Looking right, its view is not blocked; it can see 2 trees.
+Looking down, its view is blocked eventually; it can see 2 trees (one of height 3, then the tree of height 5 that blocks its view).
+A tree's scenic score is found by multiplying together its viewing distance in each of the four directions. For this tree, this is 4 (found by multiplying 1 * 1 * 2 * 2).
+
+However, you can do even better: consider the tree of height 5 in the middle of the fourth row:
+
+30373
+25512
+65332
+33549
+35390
+Looking up, its view is blocked at 2 trees (by another tree with a height of 5).
+Looking left, its view is not blocked; it can see 2 trees.
+Looking down, its view is also not blocked; it can see 1 tree.
+Looking right, its view is blocked at 2 trees (by a massive tree of height 9).
+This tree's scenic score is 8 (2 * 2 * 1 * 2); this is the ideal spot for the tree house.
+
+Consider each tree on your map. What is the highest scenic score possible for any tree?*/
+namespace Day_8
 {
     internal static class puzzle2
     {
-        public static void main(string puzzleData) 
+        public static void main(string puzzleData)
         {
-
+            string[] gridLines = puzzleData.Split("\r\n");
+            List<List<int>> grid = new List<List<int>>();//gets grid of numbers from puzzle data
+            foreach (string line in gridLines)
+            {
+                List<int> toAdd = new List<int>();
+                char[] characters = line.ToCharArray();
+                foreach (char tree in characters)
+                {
+                    toAdd.Add(int.Parse(tree.ToString()));
+                }
+                grid.Add(toAdd);
+            }
+            int bestScenic = 0;//gets total viewable trees
+            for (int i = 0; i < grid.Count; i++)
+            {
+                for (int j = 0; j < grid[i].Count; j++)
+                {
+                    int testvalue = getScenicScore(grid, i, j);
+                    if (bestScenic < testvalue)
+                    {
+                        bestScenic = testvalue;
+                    }
+                }
+            }
+            Console.WriteLine("The best scenic score is: " + bestScenic);
+        }
+        public static int getScenicScore(List<List<int>> grid, int row, int col)//gets a trees scenic score
+        {
+            int upScore = 0;
+            int downScore = 0;
+            int leftScore = 0;
+            int rightScore = 0;
+            upScore=checkUp(grid, row, col);
+            downScore=checkDown(grid, row, col);
+            rightScore=checkRight(grid, row, col);
+            leftScore=checkLeft(grid, row, col);
+            return upScore*downScore*leftScore*rightScore;
+        }
+        public static int checkUp(List<List<int>> grid, int row, int col) //counts until a tree is blocked
+        {
+            if (row != 0)
+            {
+                int treeCount = 0;
+                for (int i = row - 1; i != -1; i--)
+                {
+                    treeCount++;
+                    if (grid[i][col] >= grid[row][col])
+                    {
+                        break;
+                    }
+                }
+                return treeCount;
+            }
+            return 0;
+        }
+        public static int checkDown(List<List<int>> grid, int row, int col)//checks down to see if view is blocked
+        {
+            if (row != grid.Count)
+            {
+                int treeCount = 0;
+                for (int i = row+1; i != grid.Count; i++)
+                {
+                    treeCount++;
+                    if (grid[i][col] >= grid[row][col])
+                    {
+                        break;
+                    }
+                }
+                return treeCount;
+            }
+            return 0;
+        }
+        public static int checkLeft(List<List<int>> grid, int row, int col)//checks left to see if view is blocked
+        {
+            if (col != 0)
+            {
+                int treeCount = 0;
+                for (int j = col - 1; j != -1; j--)
+                {
+                    treeCount++;
+                    if (grid[row][j] >= grid[row][col])
+                    {
+                        break;
+                    }
+                }
+                return treeCount;
+            }
+            return 0;
+        }
+        public static int checkRight(List<List<int>> grid, int row, int col)//checks right to see if view is blocked
+        {
+            if (col != grid[row].Count)
+            {
+                int treeCount = 0;
+                for (int j = col + 1; j != grid[row].Count; j++)
+                {
+                    treeCount++;
+                    if (grid[row][j] >= grid[row][col])
+                    {
+                        break;
+                    }
+                }
+                return treeCount;
+            }
+            return 0;
         }
     }
 }
